a264c67323ecc4b7988672b702e168584043ce72
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2020-08-21 13:39:15 -0500

    mobile submenu nav

--- scripts.js
+++ scripts.js
@@ -7,3 +7,21 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+function Submenu() {
+  var x = document.getElementById("dropdown1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function Submenu2() {
+  var x = document.getElementById("dropdown2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
